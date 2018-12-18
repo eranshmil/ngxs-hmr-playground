5c1724d52b8cca679936853afc2efbbe3c1c24c5
eranshmil <eranshmil@users.noreply.github.com>
2018-12-18 19:23:29 +0200

    add hmr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,41 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
+
+import { NgxsHmrLifeCycle, NgxsStoreSnapshot } from '@ngxs/hmr-plugin';
+import { StateOperations } from '@ngxs/store';
+
+import { environment } from '../environments/environment';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production,
+    }),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
+    NgxsRouterPluginModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule implements NgxsHmrLifeCycle<NgxsStoreSnapshot> {
+  public hmrNgxsStoreOnInit(ctx: StateOperations<NgxsStoreSnapshot>, snapshot: NgxsStoreSnapshot) {
+    console.log('[NGXS HMR] Current state', ctx.getState());
+    console.log('[NGXS HMR] Previous state', snapshot);
+    ctx.setState({ ...ctx.getState(), ...snapshot });
+  }
+
+  public hmrNgxsStoreBeforeOnDestroy(ctx: StateOperations<NgxsStoreSnapshot>): NgxsStoreSnapshot {
+    const snapshot: NgxsStoreSnapshot = ctx.getState();
+    console.log('[NGXS HMR] Saved state before on destroy', snapshot);
+    return snapshot;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,21 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { hmrNgxsBootstrap } from '@ngxs/hmr-plugin';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+if (environment.hmr) {
+  if (module['hot']) {
+    hmrNgxsBootstrap(module, bootstrap, false);
+  } else {
+    console.error('HMR is not enabled for webpack-dev-server!');
+    console.log('Are you using the --hmr flag for ng serve?');
+  }
+} else {
+  bootstrap().catch(err => console.log(err));
+}
